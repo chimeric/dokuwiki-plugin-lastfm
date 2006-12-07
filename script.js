331bf48331ee5ccf96ddfb8efe12f7e3345ee081
Michael Klier <chi@chimeric.de>
2006-12-07 19:54:50 +0100

    added missing script.js
    darcs-hash:20061207185450-23886-fe17d8802fadc5c1799cbf3b6235535db56bc5de.gz

---
+++ script.js
@@ -0,0 +1,54 @@
+/**
+ * javascript functionality for the lastfm plugin
+ *
+ * @author Michael Klier <chi@chimeric.de>
+ */
+
+/**
+ * performs the ajax call
+ *
+ * @author Michael Klier <chi@chimeric.de>
+ */ 
+function lastfm_ajax(obj){
+    if(!document.getElementById) return;
+    if(!obj) return;
+
+    // We use SACK to do the AJAX requests
+    var ajax = new sack(DOKU_BASE+'lib/plugins/lastfm/ajax.php');
+    ajax_qsearch.sack.AjaxFailedAlert = '';
+    ajax_qsearch.sack.encodeURIString = false;
+
+    // show loadbar
+    lastfm_loader(obj);
+ 
+    // define callback
+    ajax.onCompletion = function(){
+        var data = this.response;
+        if(data === ''){ return; }
+        obj.style.visibility = 'hidden';
+        obj.innerHTML = data;
+        obj.style.visibility = 'visible';
+    };
+
+    ajax.runAJAX('user='+plugin_lastfm_user+'&chart='+obj.id+'&limit='+plugin_lastfm_limit+'&dformat='+plugin_lastfm_dformat+'&utc_offset='+plugin_lastfm_utc_offset);
+}
+
+/**
+ * shows the loading image
+ *
+ * @author Michael KLier <chi@chimeric.de>
+ */
+function lastfm_loader(obj) {
+    if(!obj) return;
+    obj.innerHTML = '<img src="'+DOKU_BASE+'lib/plugins/lastfm/images/loader.gif" />'; 
+}
+
+// add the init event
+addInitEvent(function() {
+    var obj = $('plugin_lastfm');
+    if(!obj) return;
+    var charts = getElementsByClass('plugin_lastfm_chart',obj,'div');
+    for(var i=0;i<charts.length;i++) {
+        lastfm_ajax(charts[i]);
+    }
+});
