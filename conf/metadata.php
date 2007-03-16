cae53259c03289d877dc41ed26fb1ee8c37ccba5
Michael Klier <chi@chimeric.de>
2007-03-16 22:30:21 +0100

    fixed regex in utc_offset to allow 0 too
    darcs-hash:20070316213021-23886-db187955e25ac6af8d314e908ed2f7f2b04b0348.gz

--- conf/metadata.php
+++ conf/metadata.php
@@ -2,5 +2,5 @@
 /**
  * Metadata of the LastFM Plugin for the configuration manager
  */
-$meta['utc_offset'] = array('string','_pattern' => '#^[+|-][1-9]{1,2}$#');
+$meta['utc_offset'] = array('string','_pattern' => '#^([+|-][1-9]{1,2}|0)$#');
 $meta['dformat']    = array('string');
